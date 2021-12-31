ca4da5cced2bdb6faf12eeade8f848084f132ca0
Deepak-iiitg <94068572+Deepak-iiitg@users.noreply.github.com>
2021-12-31 15:11:59 +0530

    Add files via upload

---
+++ Assn2_q1.js
@@ -0,0 +1,12 @@
+var a = prompt();
+var b = prompt();
+var c = prompt();
+if (a<b && a<c){
+	console.log("a is minimum");
+}
+else if ( b<a && b<c){
+	console.log("b is minimum");
+}
+else{
+	console.log("c is minimum");
+}
---
+++ Assn2_q2.js
@@ -0,0 +1,7 @@
+var c = prompt();
+if (c=='a' || c=='A' || c=='e' || c=='E' || c=='o' || c=='O' || c=='i' || c=='I' || c=='u' || c=='U'){
+	console.log("character c is vowel");
+} 
+else{
+	console.log("character c is not vowel");
+}
